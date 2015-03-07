4187cd849818873c35454c25dfd2af3564247276
Sebastian Larsson <sebastian.larsson@cybercom.com>
2015-03-07 11:43:22 +0100

    created setup to remove duplication

--- src/test/java/pos/PointOfSaleAppTest.java
+++ src/test/java/pos/PointOfSaleAppTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PointOfSaleAppTest {
+
+	private Display display;
+	private PointOfSaleApp pointOfSaleApp;
 
 	// Test list
 	// *. invalid ("") bar code, displays nothing ("")
@@ -14,28 +18,25 @@ public class PointOfSaleAppTest {
 	// *. invalid (null) bar code, displays nothing ("")
 	// *. bar code ("9999") not found, displays price not found ("NOT FOUND)
 
-	@Test
-	public void empty_barcode_displays_nothing() {
-		Display display = new Display();
-		@SuppressWarnings("serial")
-		PointOfSaleApp pointOfSaleApp = new PointOfSaleApp(display, new HashMap<String, String>() {
+	@Before
+	@SuppressWarnings("serial")
+	public void createApp() {
+		display = new Display();
+		pointOfSaleApp = new PointOfSaleApp(display, new HashMap<String, String>() {
 			{
 				put("12345", "$7.95");
 			}
 		});
+	}
+
+	@Test
+	public void empty_barcode_displays_nothing() {
 		pointOfSaleApp.onBarcode("");
 		assertEquals("", display.getLastMessage());
 	}
-	
+
 	@Test
 	public void finds_barcode_then_displays_price() {
-		Display display = new Display();
-		@SuppressWarnings("serial")
-		PointOfSaleApp pointOfSaleApp = new PointOfSaleApp(display, new HashMap<String, String>() {
-			{
-				put("12345", "$7.95");
-			}
-		});
 		pointOfSaleApp.onBarcode("12345");
 		assertEquals("$7.95", display.getLastMessage());
 	}
